5f482962e1f74d544b99c501913bfa6a8883d265
UOSjc <jjle0347@gmail.com>
2023-03-17 16:16:25 +0900

    Update week3.c

--- cpro/cpro/week3.c
+++ cpro/cpro/week3.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-	
+
 	char name[15];
-	char dep[30];
 	int birth;
+	int stud_num;
+	char depart[40];
 
 	printf("이름 입력 : ");
-	scanf_s("%s", &name, sizeof(name));
-	printf("학과 명 입력 : ");
-	scanf_s("%s", &dep, sizeof(dep));
-	printf("생일 입력 : ");
+	scanf_s("%s", name, sizeof(name));
+	printf("생년월일(8자리) 입력 : ");
 	scanf_s("%d", &birth);
+	printf("학번 입력 : ");
+	scanf_s("%d", &stud_num);
+	printf("학과명 입력 : ");
+	scanf_s("%s", depart, sizeof(depart));
 
-	printf("이름 : %s\n학과 명 : %s\n생일 : %d\n", name, dep, birth);
-
+	printf("이름 : %s\n생년월일 : %d\n학번 : %d\n학과명 : %s", name, birth, stud_num, depart);
+  
 }
